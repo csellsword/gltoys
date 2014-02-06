d8a9a4d27435ce67854fd107cd0c70d81eebc3ee
Michael Riedlin <mriedlin@gmail.com>
2014-02-05 22:11:21 -0800

    shaderutils does not compile

--- shaderutils.cpp
+++ shaderutils.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <glew.h>
 #include <string>
 
@@ -13,6 +14,32 @@ void LoadShader( const std::string& path, GLenum type )
   }
 
   GLuint shaderObj = glCreateShader( type );
-  glShaderSource( shaderObj, 1, shaderSrc.c_str(), NULL );
+  const char* cstr = shaderSrc.c_str();
+  glShaderSource( shaderObj, 1, &cstr, NULL );
   glCompileShader( shaderObj );
+
+  // logging
+  glGetShaderiv( shaderObj, GL_COMPILE_STATUS, &result );
+  glGetShaderiv( shaderObj, GL_INFO_LOG_LENGTH, &infologlength );
+  if( infologlength > 0 )
+  {
+    // print some shit, defered to later
+  }
+
+  // linking
+  GLuint program = glCreateProgram();
+  glAttachShader( program, shaderObj );
+  glLinkProgram( program ); // this should probably be a separate function
+
+  // log the linking
+  glGetProgramiv( program, GL_LINK_STATUS, &result );
+  glGetProgramiv( program, GL_INFO_LOG_LENGTH, &infologlength );
+  if( infologlength > 0 )
+  {
+    // print some shit
+  }
+
+  glDeleteShader( shaderObj ); // certainly don't need the obj anymore
+
+  return program; // need to actually return the shader
 }
